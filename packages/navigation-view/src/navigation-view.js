b75e61d337364bd3df69d56ec6c79b38a6a1a013
alex8088 <244096523@qq.com>
2020-07-07 19:03:11 +0800

    pref: navigation-view support keep-alive

--- packages/navigation-view/src/navigation-view.js
+++ packages/navigation-view/src/navigation-view.js
@@ -1,5 +1,8 @@
 export default {
   name: 'WxNavigationView',
+  props: {
+    keepAlive: [String, RegExp]
+  },
   data() {
     return {
       historyMap: {},
@@ -16,12 +19,22 @@ export default {
         this.historyMap[_to] = false
         this.transitionName = 'back'
       } else {
-        this.historyMap[_from || '/'] = true
+        this.historyMap[_from || '/'] = _to !== '/'
         this.transitionName = 'forward'
       }
     }
   },
   render(h) {
-    return h('transition', { props: { name: this.transitionName } }, [h('router-view', { class: 'weui-page' })])
+    if (this.keepAlive) {
+      return (
+        <transition name={this.transitionName}>
+          <keep-alive include={this.keepAlive}>
+            <router-view class="weui-page"></router-view>
+          </keep-alive>
+        </transition>
+      )
+    } else {
+      return h('transition', { props: { name: this.transitionName } }, [h('router-view', { class: 'weui-page' })])
+    }
   }
 }
